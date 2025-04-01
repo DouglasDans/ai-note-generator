edc548e85dd26391e0c412f9e28b1685dc73eeb9
douglasdans <douglas.dans@gmail.com>
2025-04-01 14:40:12 -0300

    feat: adiciona interfaces TypeScript para estrutura de resposta JSON

---
+++ ui-client/src/@types/JsonResponse.ts
@@ -0,0 +1,36 @@
+interface Disciplina {
+  nome: string;
+  professor: string;
+  aulas?: Aula[];
+}
+
+export interface Aula {
+  id?: string;
+  data: string;
+  titulo: string;
+  resumo: string;
+  off_topic: string;
+  tarefas_futuras: TarefasFuturas;
+  datas_futuras_mencionadas: DataFutura[];
+  atividades_em_aula: string;
+  tags: string[];
+}
+
+interface TarefasFuturas {
+  descricao_geral: string;
+  detalhes: TarefaDetalhe[];
+}
+
+interface TarefaDetalhe {
+  titulo: string;
+  descricao: string;
+}
+
+interface DataFutura {
+  data: string;
+  descricao: string;
+}
+
+export interface Disciplinas {
+  disciplinas: Disciplina[];
+}
