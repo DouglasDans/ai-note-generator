378742f8df6269d5f86c4ea8f6beb29d8ca827ee
douglasdans <douglas.dans@gmail.com>
2025-04-03 07:51:56 -0300

    feat: substitui a lib react-markdown pela lib next-mdx para denderização de markdown

--- ui-client/next.config.ts
+++ ui-client/next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
--- ui-client/src/components/aula-content/index.tsx
+++ ui-client/src/components/aula-content/index.tsx
@@ -1,15 +1,16 @@
-import {Fragment} from "react";
+import { Fragment } from "react";
 import styles from "./index.module.scss";
-import {Card, CardContent, Chip, Typography} from "@mui/joy";
+import { Card, CardContent, Chip, Typography } from "@mui/joy";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import {Aula} from "@/types/JsonResponse";
+import { Aula } from "@/types/JsonResponse";
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 type Props = {
 	aula: Aula
 }
 
-export default async function AulaContent({ aula } : Props) {
+export default async function AulaContent({ aula }: Props) {
 	return (
 		<Fragment>
 			<h2 id="tarefas-futuras">Tarefas Futuras</h2>
@@ -20,7 +21,7 @@ export default async function AulaContent({ aula } : Props) {
 						<Card variant="soft" color="danger" key={index}>
 							<CardContent>
 								<Typography level="title-md">{item.titulo}</Typography>
-								<Markdown remarkPlugins={[remarkGfm]}>{item.descricao}</Markdown>
+								<MDXRemote source={item.descricao} />
 							</CardContent>
 						</Card>
 					)
@@ -32,28 +33,27 @@ export default async function AulaContent({ aula } : Props) {
 			<div className={styles.cardContainer}>
 				{aula.datas_futuras_mencionadas.map((item, index) => {
 					return (
-							<Card variant="soft" color="warning" key={index}>
-								<CardContent>
-									<Typography level="title-md">{item.data}</Typography>
-									<Markdown remarkPlugins={[remarkGfm]}>{item.descricao}</Markdown>
-								</CardContent>
-							</Card>
+						<Card variant="soft" color="warning" key={index}>
+							<CardContent>
+								<Typography level="title-md">{item.data}</Typography>
+								<MDXRemote source={item.descricao} />
+							</CardContent>
+						</Card>
 					)
 				})}
 			</div>
 			<hr />
 
 			<h2 id="resumo">Resumo da Aula</h2>
-			<Markdown remarkPlugins={[remarkGfm]}>{aula.resumo}</Markdown>
+			<MDXRemote source={aula.resumo} />
 			<hr />
 
-
 			<h2 id="atividades">Atividades Realizadas em Aula</h2>
-			<Markdown remarkPlugins={[remarkGfm]}>{aula.atividades_em_aula}</Markdown>
+			<MDXRemote source={aula.atividades_em_aula} />
 			<hr />
 
 			<h2 id="off-topic">Discussões Off Topic</h2>
-			<Markdown remarkPlugins={[remarkGfm]}>{aula.off_topic}</Markdown>
+			<MDXRemote source={aula.off_topic} />
 			<hr />
 
 			<h2 id="tags">Tags</h2>
