0e6139d5a76230fafbd3e99ce8e1b15a2a062686
douglasdans <douglas.dans@gmail.com>
2025-04-27 10:35:42 -0300

    feat: substitui chamada ao Firebase por leitura de dados do arquivo JSON

--- ui-client/src/app/page.tsx
+++ ui-client/src/app/page.tsx
@@ -1,14 +1,23 @@
 import LinkList from "@/components/link-list";
-import { getAllDisciplinas } from "@/services/firebase.service";
 import { Fragment } from "react";
+import json_db from '../../../generated_notes_db.json'
+import { DisciplinaCollection } from "@/types/collectionTypes";
 
 export default async function Home() {
-  const disciplinas = await getAllDisciplinas();
+  const lengthJson = Object.keys(json_db.disciplinas).length
+  const disciplinas: Array<DisciplinaCollection> = []
+
+  for (let i = 1; i < lengthJson; i++) {
+    if (json_db.disciplinas[i.toString()]) {
+      disciplinas.push(json_db.disciplinas[i.toString()])
+    }
+  }
 
   return (
     <Fragment>
       <h1>Disciplinas registradas</h1>
       <hr />
+
       <LinkList data={disciplinas} />
     </Fragment>
   );
